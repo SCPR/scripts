41c429536245bdaed4445c53e05ebc5b63143955
Bryan Ricker <bricker88@gmail.com>
2013-08-28 11:59:41 -0700

    Bring over CSV scripts from SCPRv4

---
+++ SCPRv4/entries_by_blog.rb
@@ -0,0 +1,30 @@
+require "csv"
+
+filename = "represent"
+
+blogs = [
+  "politics"
+]
+
+Rails.application.default_url_options[:host] = "http://scpr.org"
+
+low  = Time.new(2013, 7, 1, 0, 0, 0) - 1
+high = Time.new(2013, 7, 31, 0, 0, 0) - 1
+
+#------------------
+
+rows = []
+
+blogs.each do |blog|
+  blog = Blog.find_by_slug(blog)
+
+  blog.entries.where("published_at > :low and published_at <= :high", low: low, high: high).published.reorder("published_at").each do |entry|
+    rows.push [entry.published_at, entry.to_title, entry.byline, entry.public_url]
+  end
+end
+
+CSV.open(Rails.root.join("log", "#{filename}-#{Time.now.strftime("%F")}.csv"), "a+") do |csv|
+  rows.each do |row|
+    csv << row
+  end
+end
