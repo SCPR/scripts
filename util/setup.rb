639922cb11a42a8ec6e6ed63b3ef37e224f9656e
Bryan Ricker <bricker88@gmail.com>
2013-09-04 14:32:26 -0700

    Setup default_url_options in setup.rb

--- util/setup.rb
+++ util/setup.rb
@@ -1,2 +1,8 @@
 require File.join(ENV['PROJECT_HOME'], APP, 'config', 'environment')
 require File.expand_path('../gist_upload', __FILE__)
+
+URLS = {
+  "scprv4" => "scpr.org"
+}
+
+Rails.application.default_url_options[:host] = URLS[APP]
