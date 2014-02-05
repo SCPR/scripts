5e5c706751ed82277e3885bd8340e81aadca13f3
Bryan Ricker <bricker88@gmail.com>
2014-02-05 14:59:08 -0800

    Script cleanup

--- streammachine/generate_soundexchange_reports.sh
+++ streammachine/generate_soundexchange_reports.sh
@@ -1,5 +1,7 @@
 # Run the setup script (see _setup.sh for more details)
 . _setup.sh
 
+# Usage:
+# ./generate_soundexchange_reports.sh
 echo "Generating SoundExchange report..."
-ruby -W0 ${SRC_DIR}/ruby_scripts/parse_w3c_logs_for_soundexchange.rb $FULL_LOG
+ruby -W0 ${SRC_DIR}/ruby_scripts/parse_w3c_logs_for_soundexchange.rb $FULL_LOG > ${PARSED_DIR}/StreamMachine-SoundExchange-${FN_DATE}.txt
