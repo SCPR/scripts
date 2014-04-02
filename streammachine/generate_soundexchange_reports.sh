aaf18a55fc76273a7367a11a9e31461108dd91d7
Bryan Ricker <bricker88@gmail.com>
2014-04-02 14:10:13 -0700

    Fix soundexchange script

--- streammachine/generate_soundexchange_reports.sh
+++ streammachine/generate_soundexchange_reports.sh
@@ -4,4 +4,4 @@
 # Usage:
 # ./generate_soundexchange_reports.sh
 echo "Generating SoundExchange report..."
-ruby -W0 ${SRC_DIR}/ruby_scripts/parse_w3c_logs_for_soundexchange.rb $FULL_LOG > ${PARSED_DIR}/StreamMachine-SoundExchange-${FN_DATE}.txt
+ruby -W0 ${SRC_DIR}/ruby_scripts/parse_w3c_logs_for_soundexchange.rb $FULL_LOG
