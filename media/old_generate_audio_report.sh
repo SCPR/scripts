28b543ed4e0bbe201d5642141293b00a9d9645d5
Bryan Ricker <bricker88@gmail.com>
2014-04-03 15:42:29 -0700

    Remove old parsing scripts

--- media/old_generate_audio_report.sh
+++
@@ -1,7 +0,0 @@
-# Run the setup script (see _setup.sh for more details)
-. _setup.sh
-
-# Usage:
-# ./old_generate_audio_report.sh logs/media-access.log audio_2014-01.csv
-echo "Generating OLD Audio report..."
-ruby -W0 ${SRC_DIR}/ruby_scripts/old_parse_media_log.rb $1 > ${PARSED_DIR}/$2
